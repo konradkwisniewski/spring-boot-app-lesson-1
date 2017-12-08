9e62646ee42d55cac10f08f86fcd776cf43bafe3
Konrad Wiśniewski <konrad.name Konrad>
2017-12-08 18:35:15 +0100

    Initial commit

---
+++ src/main/java/com/appfactory/springlesson1/SpringLesson1Application.java
@@ -0,0 +1,12 @@
+package com.appfactory.springlesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLesson1Application.class, args);
+	}
+}
---
+++ src/test/java/com/appfactory/springlesson1/SpringLesson1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appfactory.springlesson1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLesson1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
